dd31ce839b8df2d65af45f32240c34ff64367d06
coderannu <akrajput01206@gmail.com>
2023-06-03 11:59:36 +0530

    Add Signup form

--- app/Views/Auth/SignUp/SignUp.php
+++ app/Views/Auth/SignUp/SignUp.php
@@ -1,10 +1,13 @@
+<!-- Start Register Section  -->
+<!-- Start Main Section  -->
 <section class="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
     <div class="container">
         <p class="text-center">
-            <a href="../dashboard/dashboard.html" class="d-flex align-items-center justify-content-center">
+            <a href="#!/" class="d-flex align-items-center justify-content-center">
                 <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path>
-                </svg> Back to homepage</a>
+                </svg> Back to homepage
+            </a>
         </p>
         <div class="row justify-content-center form-bg-image" data-background-lg="../../assets/img/illustrations/signin.svg" style="background: url(&quot;../../assets/img/illustrations/signin.svg&quot;);">
             <div class="col-12 d-flex align-items-center justify-content-center">
@@ -12,7 +15,19 @@
                     <div class="text-center text-md-center mb-4 mt-md-0">
                         <h1 class="mb-0 h3">Create Account</h1>
                     </div>
-                    <form action="#" class="mt-4">
+                    <form ng-submit="add_user(user);" class="mt-4">
+                    <div class="form-group mb-4">
+                            <label for="name">Your Name</label>
+                            <div class="input-group">
+                                <span class="input-group-text" id="basic-addon1">
+                                    <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                                        <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                                    </svg>
+                                </span>
+                                <input type="text" ng-model="user.name" class="form-control" placeholder="enter your name" id="email" autofocus="" required="">
+                            </div>
+                        </div>
                         <div class="form-group mb-4">
                             <label for="email">Your Email</label>
                             <div class="input-group">
@@ -22,7 +37,7 @@
                                         <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
                                     </svg>
                                 </span>
-                                <input type="email" class="form-control" placeholder="example@company.com" id="email" autofocus="" required="">
+                                <input type="email" ng-model="user.email" class="form-control" placeholder="example@company.com" id="email" autofocus="" required="">
                             </div>
                         </div>
                         <div class="form-group">
@@ -34,20 +49,10 @@
                                             <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path>
                                         </svg>
                                     </span>
-                                    <input type="password" placeholder="Password" class="form-control" id="password" required="">
+                                     <input type="password" ng-model="user.password" placeholder="Password" class="form-control" id="password" required="">
                                 </div>
                             </div>
-                            <div class="form-group mb-4">
-                                <label for="confirm_password">Confirm Password</label>
-                                <div class="input-group">
-                                    <span class="input-group-text" id="basic-addon2">
-                                        <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path>
-                                        </svg>
-                                    </span>
-                                    <input type="password" placeholder="Confirm Password" class="form-control" id="confirm_password" required="">
-                                </div>
-                            </div>
+
                             <div class="mb-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" value="" id="remember">
@@ -83,4 +88,5 @@
             </div>
         </div>
     </div>
-</section>
+</section> <!-- End Main Section  -->
+<!-- End Register Section  -->
