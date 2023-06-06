77e178f9cbc13ff5a3fbb510c204c8192a12a525
coderannu <akrajput01206@gmail.com>
2023-06-06 12:53:30 +0530

    Add Helper function

--- app/Helpers/auth_helper.php
+++ app/Helpers/auth_helper.php
@@ -2,22 +2,25 @@
 
 
 // Authentication Helper 
-
 use App\Models\UsersModel;
 use Config\Services;
 
 
-
+// Check Session User Is login or not 
 if (!function_exists('isLoggedIn')) {
+    /**
+     * isLoggedIn
+     *
+     * @return void
+     */
     function isLoggedIn()
     {
         $session = Services::session();
         return $session->has('user');
-
-        
     }
 }
 
+// Get Store Current user Session   
 if (!function_exists('current_user')) {
     /**
      * current_user
@@ -26,14 +29,12 @@ if (!function_exists('current_user')) {
      */
     function current_user()
     {
-
         if (isLoggedIn()) {
 
             $session = Services::session();
             $id =  $session->get('user');
-
+            // get user Id from user table 
             $DB = new UsersModel();
-
             return $DB->find($id);
         }
         return null;
