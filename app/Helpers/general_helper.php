9c1390924984fbf8ed62d76837e83298179c3501
coderannu <akrajput01206@gmail.com>
2023-06-07 13:26:28 +0530

    user status helper function

--- app/Helpers/general_helper.php
+++ app/Helpers/general_helper.php
@@ -7,3 +7,17 @@
  * This is our general Helper Functions  
  * 
  **************************/
+
+//  Get user Status, user active, inactive or banned  
+if (!function_exists('get_user_status')) {
+
+    /**
+     * get_user_status
+     *
+     * @return void
+     */
+    function get_user_status()
+    {
+        return ["active", "inactive", "banned"];
+    }
+}
