4fc8e7b02b01d8566ddee97e18ce03a35578a3f9
coderannu <akrajput01206@gmail.com>
2023-06-07 13:20:59 +0530

    Add Helper

--- app/Helpers/auth_helper.php
+++ app/Helpers/auth_helper.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**************************
  *  Authentication Helper
  * 
---
+++ app/Helpers/general_helper.php
@@ -0,0 +1,9 @@
+<?php
+
+/**************************
+ *  General Helper
+ * 
+ * We can use helper function globally coz ci-4 provide functionality to use helper functions globally anywhere in your application  
+ * This is our general Helper Functions  
+ * 
+ **************************/
