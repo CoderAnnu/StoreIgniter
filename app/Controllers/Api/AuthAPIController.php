9e810d0a5724991ddc00f76c1661efd486a0a544
anand <akrajput01206@gmail.com>
2023-08-22 17:58:16 +0530

    Test

--- app/Controllers/Api/AuthAPIController.php
+++ app/Controllers/Api/AuthAPIController.php
@@ -23,7 +23,7 @@ class AuthAPIController extends BaseController
 
     public function index()
     {
-        //add some test code updated update new new update 
+        //add some test code updated code updated
     }
 
     public function create()
