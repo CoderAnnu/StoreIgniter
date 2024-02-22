5646246bd881f5cc4b1bac97768ab59df8b4fa15
anand <akrajput01206@gmail.com>
2024-02-22 18:30:57 +0530

    update signup page

--- app/Views/Auth/SignUp/SignUp.php
+++ app/Views/Auth/SignUp/SignUp.php
@@ -1,7 +1,7 @@
 <!-- Start Register Section  -->
 <!-- Start Main Section  -->
 <main>
-
+    <!-- starting SignUp page  -->
     <section class="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
         <div class="container">
             <!-- <p class="text-center">
