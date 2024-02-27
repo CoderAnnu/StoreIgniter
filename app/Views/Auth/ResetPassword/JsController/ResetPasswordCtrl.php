3f49c4318d60e0a476d169304be358aa28dd2602
anand <akrajput01206@gmail.com>
2024-02-27 11:05:04 +0530

    updating Reset password functionality

--- app/Views/Auth/ResetPassword/JsController/ResetPasswordCtrl.php
+++ app/Views/Auth/ResetPassword/JsController/ResetPasswordCtrl.php
@@ -1,18 +1,15 @@
 <script>
     Auth.onlyFor('logged-out', '/');
 
-    $scope.loading = false;
-    $scope.completed = false;
     // getting reset link page by user 
     $scope.reset = (user) => {
         $scope.token = $routeParams.key;
+
         let password = user;
         let data = {
             'key': $scope.token,
             'user': password,
         };
-
-        $scope.loading = true;
 
         $http.post('../../../api/auth/reset/', data).then(
             resp => {
@@ -33,11 +30,7 @@
                 // redirect after reset password 
                 $location.path('/signin')
 
-                $scope.loading = false;
-                $scope.completed = true;
-
-            },
-            err => {
+            }, err => {
                 let notyf = new Notyf({
                     duration: 5000,
                     position: {
@@ -51,7 +44,6 @@
 
                 // Display error message Notification 
                 notyf.error(error);
-                $scope.loading = false;
             }
         )
     }
