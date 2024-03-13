12ba628407e24babdc58d7abd53983a8557f63dd
anand <akrajput01206@gmail.com>
2024-03-13 17:08:50 +0530

    Adding  edit user structure

--- app/Views/Admin/Users/EditUser/EditUser.php
+++ app/Views/Admin/Users/EditUser/EditUser.php
@@ -1,13 +1,9 @@
 <side-bar></side-bar>
 
-
-
 <main class="content">
 
     <top-bar></top-bar>
-
     <div class="container">
-
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
             <div class="d-block mb-4 mb-md-0">
                 <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
