c6f42635dd9687822d4047c50a3f27622d6b6ba4
anand <akrajput01206@gmail.com>
2024-03-13 12:52:39 +0530

    update dashboard functionality

--- app/Views/Admin/Dashboard/Dashboard.php
+++ app/Views/Admin/Dashboard/Dashboard.php
@@ -13,7 +13,7 @@
 
           <!-- Content wrapper -->
           <div class="content-wrapper">
-            admin test 
+            admin test
             <!--  -->
             <div class="container-xxl flex-grow-1 container-p-y">
               <h4 class="py-3 mb-4"><span class="text-muted fw-light">UI elements /</span> Modals</h4>
