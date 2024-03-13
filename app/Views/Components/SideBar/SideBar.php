a8ef70f9a71c3b31cb44692d0e9af1972e9c5218
anand <akrajput01206@gmail.com>
2024-03-13 12:53:48 +0530

    update side bar adding customer and order modules

--- app/Views/Components/SideBar/SideBar.php
+++ app/Views/Components/SideBar/SideBar.php
@@ -14,47 +14,18 @@
   <nav id="sidebarMenu" class="sidebar d-lg-block collapse my-5" data-simplebar>
     <ul class="menu-inner nav flex-column">
       <li class="nav-item menu-item" ng-class='{"active" : $ctrl.path == "/"}'>
-        <a href="#!/" class="nav-link menu-link m-0" ng-class='{"menu-link" : $ctrl.path == "/"}'>
-          <i class="me-3 fa-solid fa-house"></i>
+        <a href="#!/" class="ps-3 nav-link" ng-class='{"menu-link" : $ctrl.path == "/"}'>
+          <i class="menu-icon tf-icons mdi mdi-home-outline"></i>
           <span class="sidebar-text">Dashboard</span>
         </a>
       </li>
-      <!-- Example Sub menu   -->
-      <li class="menu-item">
-        <span class="nav-link menu-link d-flex justify-content-between align-items-center" data-bs-toggle="collapse" data-bs-target="#test">
-          <span>
-            <span>
-              <i class="me-3 fa-solid fa-robot"></i>
-            </span>
-            <span class="sidebar-text">Test</span>
-          </span>
-          <span class="link-arrow">
-            <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
-            </svg>
-          </span>
-        </span>
-        <div class="multi-level collapse" ng-class='{"show" : ["/test", "/test/testnew"].includes($ctrl.path)}' role="list" id="test" aria-expanded="false">
-          <ul class="flex-column nav">
-            <li class="nav-item" ng-class='{"active" : $ctrl.path == "/test"}'>
-              <a href="#!/bots" class="menu-link">
-                <i class="me-3 fa-solid fa-robot"></i>
-                <span class="sidebar-text">My Test</span>
-              </a>
-            </li>
-            <li class="nav-item" ng-class='{"active" : $ctrl.path == "/test/testnew" }'>
-              <a href="#!/bots/add" class="menu-link">
-                <i class="me-3 fa-solid fa-plus"></i>
-                <span class="sidebar-text">Add New Test</span>
-              </a>
-            </li>
-          </ul>
-        </div>
+      <!-- E-Commerce Section  -->
+      <li class="menu-header fw-medium mt-4">
+        <span class="menu-header-text" data-i18n="eCommerce"> eCommerce</span>
       </li>
-      <!-- end Example Sub menu   -->
       <!-- Product menu bar  -->
-      <li class="menu-item">
-        <span class="nav-link menu-link d-flex justify-content-between align-items-center" data-bs-toggle="collapse" data-bs-target="#product">
+      <li class="nav-item  menu-item">
+        <span class="nav-link menu-link d-flex justify-content-between align-items-center" ng-class='{"m-menu" : ["/product", "/product/add", "/product/catelist"].includes($ctrl.path)}' data-bs-toggle="collapse" data-bs-target="#product">
           <span>
             <span>
               <i class="me-3 fa-solid fa-bag-shopping"></i>
@@ -67,21 +38,21 @@
             </svg>
           </span>
         </span>
-        <div class="multi-level collapse" ng-class='{"show" : ["/product", "/product/addProduct"].includes($ctrl.path)}' role="list" id="product" aria-expanded="false">
+        <div class="multi-level collapse" ng-class='{"show" : ["/product", "/product/add", "/product/catelist"].includes($ctrl.path)}' role="list" id="product" aria-expanded="false">
           <ul class="flex-column nav">
-            <li class="nav-item" ng-class='{"active" : $ctrl.path == "/product"}'>
-              <a href="#!/product" class="menu-link">
-              <i class="me-3 fa-regular fa-clipboard"></i>
-              <span class="sidebar-text">Product List</span>
+            <li class="nav-item menu-item" ng-class='{"active" : $ctrl.path == "/product"}'>
+              <a href="#!/product" class="nav-link" ng-class='{"menu-link" : $ctrl.path == "/product"}'>
+                <i class="me-3 fa-regular fa-clipboard"></i>
+                <span class="sidebar-text">Product List</span>
               </a>
             </li>
-            <li class="nav-item" ng-class='{"active" : $ctrl.path == "/product/add" }'>
-              <a href="#!/product/add" class="menu-link">
+            <li class="nav-item menu-item" ng-class='{"active" : $ctrl.path == "/product/add" }'>
+              <a href="#!/product/add" class="nav-link" ng-class='{"menu-link" : $ctrl.path == "/product/add"}'>
                 <i class="me-3 fa-solid fa-plus"></i>
                 <span class="sidebar-text">Add Product</span>
               </a>
             </li>
-            <li class="nav-item" ng-class='{"active" : $ctrl.path == "/product/catelist" }'>
+            <li class="nav-item menu-item" ng-class='{"active" : $ctrl.path == "/product/catelist" }'>
               <a href="#!/product/catelist" class="menu-link">
                 <i class="me-3 fa-solid fa-layer-group"></i>
                 <span class="sidebar-text">Category List</span>
@@ -90,15 +61,103 @@
           </ul>
         </div>
       </li>
+      <!-- Order  -->
+      <li class="nav-item  menu-item">
+        <span class="nav-link menu-link d-flex justify-content-between align-items-center" ng-class='{"m-menu" : ["/product/order", "/product/order/details"].includes($ctrl.path)}' data-bs-toggle="collapse" data-bs-target="#order">
+          <span>
+            <span>
+              <i class="menu-icon mdi mdi-order-bool-descending-variant"></i>
+            </span>
+            <span class="sidebar-text">Order</span>
+          </span>
+          <span class="link-arrow">
+            <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+            </svg>
+          </span>
+        </span>
+        <div class="multi-level collapse" ng-class='{"show" : ["/product/order" , "/product/order/details"].includes($ctrl.path)}' role="list" id="order" aria-expanded="false">
+          <ul class="flex-column nav">
+            <li class="nav-item menu-item" ng-class='{"active" : $ctrl.path == "/product/order"}'>
+              <a href="#!/product/order" class="nav-link" ng-class='{"menu-link" : $ctrl.path == "/product/order"}'>
+                <i class="me-3 fa-regular tf-icons fa-clipboard"></i>
+                <span class="sidebar-text">Order List</span>
+              </a>
+            </li>
+            <li class="nav-item menu-item" ng-class='{"active" : $ctrl.path == "/product/order/details" }'>
+              <a href="#!/product/order/details" class="nav-link" ng-class='{"menu-link" : $ctrl.path == "/product/order/details"}'>
+                <i class="me-3 fa-regular tf-icons fa-clipboard"></i>
+                <span class="sidebar-text">Order Details</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <!-- end order section  -->
+
+      <!-- Customer Details  -->
+      <li class="nav-item  menu-item">
+        <span class="nav-link menu-link d-flex justify-content-between align-items-center" ng-class='{"m-menu" : ["/product/customer", "/product/customer/details"].includes($ctrl.path)}' data-bs-toggle="collapse" data-bs-target="#customer">
+          <span>
+            <span>
+              <i class="menu-icon tf-icons mdi mdi-text-account"></i>
+            </span>
+            <span class="sidebar-text">Customer</span>
+          </span>
+          <span class="link-arrow">
+            <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+            </svg>
+          </span>
+        </span>
+        <div class="multi-level collapse" ng-class='{"show " : ["/product/customer" , "/product/customer/details"].includes($ctrl.path)}' role="list" id="customer" aria-expanded="false">
+          <ul class="flex-column nav">
+            <li class="nav-item menu-item" ng-class='{"active" : $ctrl.path == "/product/customer"}'>
+              <a href="#!/product/customer" class="nav-link" ng-class='{"menu-link" : $ctrl.path == "/product/customer"}'>
+                <i class="me-3 fa-regular fa-clipboard"></i>
+                <span class="sidebar-text">All Customer</span>
+              </a>
+            </li>
+            <li class="nav-item menu-item" ng-class='{"active" : $ctrl.path == "/product/customer/details" }'>
+              <a href="#!/product/customer/details" class="nav-link" ng-class='{"menu-link" : $ctrl.path == "/product/customer/details"}'>
+                <i class="me-3 fa-regular fa-clipboard"></i>
+                <span class="sidebar-text">Customer Details</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <!-- End customer details  -->
+
       <!-- end product product menu bar  -->
-      <!-- Single menu example  -->
+      <!-- User menu example  -->
+      <li class="menu-header fw-medium mt-4">
+        <span class="menu-header-text" data-i18n="user">User</span>
+      </li>
       <li class="nav-item menu-item" ng-class='{"active" : $ctrl.path == "/admin/users"}'>
-        <a href="#!/admin/users" class="menu-link" ng-class='{"menu-link" : $ctrl.path == "/admin/users"}'>
-          <i class="me-3 fa-solid fa-user"></i>
+        <a href="#!/admin/users" class="ps-3 menu-link" ng-class='{"menu-link" : $ctrl.path == "/admin/users"}'>
+          <i class="menu-icon tf-icons mdi mdi-account-outline"></i>
           <span class="sidebar-text">Users</span>
         </a>
       </li>
       <!-- end Single menu example  -->
+      <!-- Support Section  -->
+      <li class="menu-header fw-medium mt-4">
+        <span class="menu-header-text" data-i18n="Misc">App Support</span>
+      </li>
+      <li class="nav-item menu-item" ng-class='{"active" : $ctrl.path == "/support"}'>
+        <a href="#!/support" class="ps-3 menu-link" ng-class='{"menu-link" : $ctrl.path == "/support"}'>
+          <i class="menu-icon tf-icons mdi mdi-lifebuoy"></i>
+          <span class="sidebar-text">Support</span>
+        </a>
+      </li>
+      <li class="nav-item menu-item" ng-class='{"active" : $ctrl.path == "/documentation"}'>
+        <a href="#!/documentation" class="ps-3 menu-link" ng-class='{"menu-link" : $ctrl.path == "/documentation"}'>
+          <i class="menu-icon tf-icons mdi mdi-file-document-multiple-outline"></i>
+          <span class="sidebar-text">Documentation</span>
+        </a>
+      </li>
+      <!-- end support section  -->
     </ul>
   </nav>
   <!-- End menu  -->
