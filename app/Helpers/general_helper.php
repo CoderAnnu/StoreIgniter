194ce7dd7b3b68ba1c23a9784502f5100b433226
anand <akrajput01206@gmail.com>
2024-09-24 17:44:20 +0530

    Add Option key and value CRUD operation globally

--- app/Helpers/general_helper.php
+++ app/Helpers/general_helper.php
@@ -45,19 +45,72 @@ if (!function_exists('show_404')) {
 }
 
 
-// if project is maintenance mode then it will show maintenance mode page  
 /**
+ * if project is maintenance mode then it will show maintenance mode page  
  * show_maintenance
  *
  * @param  mixed $message
  * @return void
  */
 if (!function_exists('show_maintenance')) {
-
     function show_maintenance($message = "Maintenance Mode")
     {
         return view('error/html/production', [
             "message" => $message
         ]);
+    }
+}
+
+/** Options if we want to use options like meta key meta value functionality same like that
+ * Global function use like meta key meta value 
+ */
+if (!function_exists('update_option')) {
+    function update_option($key, $val)
+    {
+        $optionsModel = new OptionsModel();
+
+        // Check if option is already exists 
+        $exists = $optionsModel->where('option_key', $key)->first();
+
+        if ($exists) {
+            // Update Option 
+            $exists->option_value = $val;
+            $id = $exists->id;
+            $optionsModel->update($id, [
+                'option_value' => $val
+            ]);
+        } else {
+            // create option 
+            $option = new Options();
+            $option->option_key = $key;
+            $option->option_value = $val;
+            $optionsModel->save($option);
+        }
+    }
+}
+
+/** Get option 
+ * If we want to get options which we store this global function through DB
+ * 
+ */
+if (!function_exists('get_option')) {
+    function get_option($key, $default = null)
+    {
+        $optionsModel = new OptionsModel();
+        $option = $optionsModel->where('option_key', $key)->first();
+        return ($option) ? $option->option_value : $default;
+    }
+}
+
+/**
+ * delete options 
+ * 
+ * if we want to delete option data which we are using this global function
+ */
+if (!function_exists('delete_option')) {
+    function delete_option($key)
+    {
+        $optionsModel = new OptionsModel();
+        return $optionsModel->where('option_key', $key)->delete();
     }
 }
