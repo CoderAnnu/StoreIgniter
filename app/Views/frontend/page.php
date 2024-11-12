6d44f7284246eadcecc1065f4b77112002ae76ad
anand <akrajput01206@gmail.com>
2024-11-12 11:36:13 +0530

    Create view page for frontend

---
+++ app/Views/frontend/page.php
@@ -0,0 +1,23 @@
+<?= $this->extend('frontend/base') ?>
+<?= $this->section('content'); ?>
+
+<div id="contact" class="contact-area p-5">
+    <div class="container">
+        <div class=" contact-content mb-3">
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <h1> <?= $page->title ?> </h1>
+                    <ul class="breadcrumb">
+                        <li><a href="/" class="me-2"><i class="fas fa-home"></i> Home</a></li>
+                        <li> <?= $page->title ?> </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div>
+            <p> <?= $page->description ?></p>
+        </div>
+    </div>
+</div>
+
+<?php $this->endSection(); ?>
