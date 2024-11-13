54e392d974f46207cbacb75ba2de5164bbde2328
anand <akrajput01206@gmail.com>
2024-11-13 10:30:17 +0530

    Update landing page path

--- app/Controllers/Public/LandingPageController.php
+++ app/Controllers/Public/LandingPageController.php
@@ -8,7 +8,13 @@ class LandingPageController extends BaseController
 {
     public function index()
     {
-        // return view('frontend/Landing/LandingPage.php');
-        return view('frontend/base.php');
+        $data['data'] = [
+            'title' => get_option('site_name', SITE_NAME),
+            'description' => get_option('site_description', SITE_DESCRIPTION),
+            'keywords' => get_option('site_keywords', SITE_KEYWORDS),
+            'author' => get_option('site_author', SITE_AUTHOR),
+        ];
+        return view('frontend/Landing/LandingPage', $data);
+        // return view('frontend/base.php');
     }
 }
